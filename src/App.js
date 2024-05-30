29b9749c52311726ba87b2deef799feba5eff945
Nourajazbeh <dr.noorj19@gmail.com>
2024-05-30 09:05:37 +0200

    update Switcher App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import { ThemeProvider } from './context/ThemeContext';
-import './App.css';
+import './index.css'; 
 
 const App = () => {
   return (
@@ -18,3 +18,4 @@ const App = () => {
 };
 
 export default App;
+
